9d0407a323c1c8bf07a5911ad11f83ecf14763f3
Nishad Mansoor <nishadm9802@gmail.com>
2026-03-18 01:52:49 -0400

    random maze generator and csv

--- generate_maze.py
+++ generate_maze.py
@@ -23,7 +23,7 @@ def solvable(maze, start, goal):
 def make_dataset(num_mazes):
     dataset = []
     while len(dataset) < num_mazes:
-        size = random.choice([10, 20, 40])
+        size = random.choice([10, 20])
         density = random.uniform(0.2, 0.5)
         weight =  [1, 3, 5, 10]
         maze = generate_maze(size, density, weight)
@@ -45,14 +45,44 @@ def make_dataset(num_mazes):
 
         #mcts, separate due to iterations
         start_time = time.time()
-        mcts_path = mcts(maze, start, goal, iterations=2000)
+        mcts_path = mcts(maze, start, goal, iterations=200)
         end_time = time.time()
         results['mcts'] = {
             'path_cost': sum(maze[x, y] for x, y in mcts_path) if mcts_path else None,
             'time': end_time - start_time}
-        dataset.append(results)
+        working_algos = {k: v for k, v in results.items() if k!= 'mcts'}
+        best_algo = min(working_algos, key=lambda x: (working_algos[x]['path_cost'] if working_algos[x]['path_cost'] is not None else float('inf'),
+        working_algos[x]['time']
+        ))
+        dataset.append({
+            'size': size,
+            'density': density,
+            'results': results,
+            'best algorithm': best_algo
+        })
     return dataset
 
-dataset = make_dataset(5)
+dataset = make_dataset(500)
+with open ('dataset.csv', 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=[ 'size', 'density', 'bfs_cost', 'bfs_time', 'ucs_cost', 'ucs_time',
+'greedy_cost', 'greedy_time', 'astar_cost', 'astar_time',
+'mcts_cost', 'mcts_time', 'best_algorithm'])
+    writer.writeheader()
+    for data in dataset:
+        writer.writerow({
+            'size': data['size'],
+            'density': data['density'],
+            'bfs_cost': data['results']['bfs']['path_cost'],
+            'bfs_time': data['results']['bfs']['time'],
+            'ucs_cost': data['results']['ucs']['path_cost'],
+            'ucs_time': data['results']['ucs']['time'],
+            'greedy_cost': data['results']['greedy']['path_cost'],
+            'greedy_time': data['results']['greedy']['time'],
+            'astar_cost': data['results']['astar']['path_cost'],
+            'astar_time': data['results']['astar']['time'],
+            'mcts_cost': data['results']['mcts']['path_cost'],
+            'mcts_time': data['results']['mcts']['time'],
+            'best_algorithm': data['best algorithm']
+        })
 for row in dataset: 
     print(row)
